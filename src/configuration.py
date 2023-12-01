9da2b24aa313ae01e3001412e476c2dc6b977078
stepan-martynov <smartd88@gmail.com>
2023-12-01 21:54:33 +0300

    added db configurations

--- src/configuration.py
+++ src/configuration.py
@@ -2,8 +2,34 @@ from dataclasses import dataclass
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
+from sqlalchemy.engine import URL
+
 
 load_dotenv(find_dotenv())
+
+
+@dataclass
+class DataBaseConfig:
+    host: str = getenv("DB_HOST")
+    port: str = getenv("DB_PORT")
+    user: str = getenv("DB_USER")
+    password: str = getenv("DB_PASSWORD")
+    database: str = getenv("DB_NAME")
+
+    driver: str = "asyncpg"
+    database_system: str = "postgresql"
+
+    @property
+    def connection_str(self) -> str:
+        return URL.create(
+            drivername=f"{self.database_system}+{self.driver}",
+            username=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.database
+        ).render_as_string(hide_password=False)
+        
 
 @dataclass
 class BotConfig:
@@ -18,8 +44,11 @@ class DadataConfig:
 
 @dataclass
 class Configuration:
+
+    db=DataBaseConfig()
     bot = BotConfig()
     dadata = DadataConfig()
+
 
 
 config = Configuration()
@@ -27,3 +56,4 @@ config = Configuration()
 
 if __name__ == "__main__":
     print(config.bot.token)
+    print(config.db.build_connection_str())
