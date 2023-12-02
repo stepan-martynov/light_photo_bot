d1c1253c5a0a5a8d272434145f3c954d4ea1b35f
stepan-martynov <smartd88@gmail.com>
2023-12-02 12:25:44 +0300

    start config sessionmaker

--- src/db/database.py
+++ src/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine as _create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
 
 from src.configuration import config
 
-engin = create_async_engine(
-    url: URL = config.db.connection_str,
-)
+def async_engin(url: str | URL) -> AsyncEngine:
+    return _create_async_engine(url, echo: bool = config.debug)
