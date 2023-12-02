e91c1906034f47a76676e1932f5ffdb3121e9e93
stepan-martynov <smartd88@gmail.com>
2023-12-02 16:54:04 +0300

    added models to db init

---
+++ src/db/models/__init__.py
@@ -0,0 +1,9 @@
+from .base import Base
+from .agency import Agency
+from .bank_accaunt import BankAccaunt
+from .manager import Manager
+
+
+__all__ = [
+    'Base', 'Agency', 'BankAccaunt', 'Manager'
+]
