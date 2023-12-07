89a16a344e3e7a75f0035c8cf61922522c029660
stepan-martynov <smartd88@gmail.com>
2023-12-08 00:24:32 +0300

    some fixes

--- src/db/models/agency.py
+++ src/db/models/agency.py
@@ -1,17 +1,26 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import BigInteger, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
+
 class Agency(Base):
     name: Mapped[str]
-    inn: Mapped[int]
-    kpp: Mapped[int]
-    ogrn: Mapped[str]
-    opf_full: Mapped[str]
+    inn: Mapped[int] = mapped_column(BigInteger)
+    kpp: Mapped[int] = mapped_column(BigInteger)
+    ogrn: Mapped[int] = mapped_column(BigInteger)
     opf_short: Mapped[str]
+    opf_full: Mapped[str]
     address: Mapped[str]
 
     manager: Mapped["Manager"] = relationship(back_populates="agency")
     bank_accaunt: Mapped["BankAccaunt"] = relationship(back_populates="agency")
-    
+
+    # def from_dadata(self, data: dict):
+    #     self.name = data['name']
+    #     self.inn = data['inn']
+    #     self.kpp = data['kpp']
+    #     self.ogrn = data['ogrn']
+    #     self.opf_full = data['opf_full']
+    #     self.opf_short = data['opf_short']
+    #     self.address = data['address']
