9670608b745028825571e3c5aa4833330d508fc1
stepan-martynov <smartd88@gmail.com>
2023-12-08 00:42:35 +0300

    some fixes

---
+++ src/api/dadata/api_requests.py
@@ -0,0 +1,57 @@
+import asyncio
+import pprint
+
+from dadata import Dadata
+from sqlalchemy import inspect
+
+from src.configuration import config
+from src.db.database import *
+from src.db.models import Agency, Manager, BankAccaunt
+
+
+dadata = Dadata(config.dadata.token)
+
+
+def get_raw_data(inn: str) -> dict:
+    try:
+        return dadata.find_by_id("party", inn)[0]['data']
+    except Exception as e:
+        print(e)
+
+
+def get_agency(raw_data: dict) -> dict:
+
+    agency = {}
+    agency['name'] = raw_data['name']['full']
+    agency['inn'] = int(raw_data['inn'])
+    agency['kpp'] = int(raw_data['kpp'])
+    agency['ogrn'] = int(raw_data['ogrn'])
+    agency['opf_short'] = raw_data['opf']['short']
+    agency['opf_full'] = raw_data['opf']['full']
+    agency['address'] = raw_data['address']['unrestricted_value']
+    return agency
+
+
+def get_manager(raw_data: dict) -> dict:
+    manager = {}
+    manager['full_name'] = raw_data['management']['name']
+    manager['post'] = raw_data['management']['post']
+    return manager
+
+
+async def main():
+    await create_tables(async_engine=async_engine)
+    raw_data = get_raw_data("7839113181")
+    # pprint.pprint(raw_data)
+    agency = get_agency(raw_data)
+    manager = get_manager(raw_data)
+    async with asyng_session_factory() as session:
+        agency = Agency(**agency)
+        manager = Manager(**manager)
+        agency.manager = manager
+        session.add_all((agency, manager))
+        await session.commit()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- src/db/database.py
+++ src/db/database.py
@@ -1,45 +1,31 @@
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 
 import asyncio
-from sqlalchemy.ext.asyncio import create_async_engine 
+from sqlalchemy.ext.asyncio import create_async_engine
 from .models import Base
 
 from sqlalchemy.ext.asyncio import async_sessionmaker
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.engine.url import URL
 
 from src.configuration import config
 from .models import Manager, BankAccaunt
 
 
-# def create_async_engine(url: str | URL, echo: bool = config.debug) -> AsyncEngine:
-    # return _create_async_engine(url=url, echo=echo)
-
-async_engine : AsyncEngine = create_async_engine(
-     url=config.db.connection_str,
-     echo=config.debug
+async_engine: AsyncEngine = create_async_engine(
+    url=config.db.connection_str,
+    echo=config.debug
 )
 
-asyng_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
-
-# def create_session_maker(engine: AsyncEngine = None) -> sessionmaker:
-#     return sessionmaker(
-#         engine=engine or create_async_engine(config.db.connection_str),
-#         class_=AsyncSession,
-#         expire_on_commit=False
-#     )
+asyng_session_factory = async_sessionmaker(
+    async_engine, expire_on_commit=False)
 
 
 async def create_tables(async_engine):
-        async_engine.echo = False
-        async with async_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-            await conn.run_sync(Base.metadata.create_all)
-        async_engine.echo = True
+    async_engine.echo = False
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    async_engine.echo = True
 
-
-# class Database:
-    # session: AsyncSession
 
 if __name__ == "__main__":
     asyncio.run(create_tables(create_async_engine()))
