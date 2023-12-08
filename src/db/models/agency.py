449a54303cef1079f70b12c4907d2111d9177ae9
stepan-martynov <smartd88@gmail.com>
2023-12-08 17:12:14 +0300

    optimaize schemas

--- src/db/models/agency.py
+++ src/db/models/agency.py
@@ -1,17 +1,18 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import BigInteger, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
 class Agency(Base):
     name: Mapped[str]
-    inn: Mapped[int]
-    kpp: Mapped[int]
-    ogrn: Mapped[str]
+    inn: Mapped[int] = mapped_column(BigInteger)
+    kpp: Mapped[int] = mapped_column(BigInteger)
+    ogrn: Mapped[int] = mapped_column(BigInteger)
     opf_full: Mapped[str]
     opf_short: Mapped[str]
     address: Mapped[str]
+    paymant_account: Mapped[int | None] = mapped_column(BigInteger)
+    cor_account: Mapped[int | None] = mapped_column(BigInteger)
 
     manager: Mapped["Manager"] = relationship(back_populates="agency")
     bank_accaunt: Mapped["BankAccaunt"] = relationship(back_populates="agency")
-    
--- src/db/models/bank_accaunt.py
+++ src/db/models/bank_accaunt.py
@@ -1,14 +1,13 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
+from sqlalchemy import BigInteger, ForeignKey
 
 from .base import Base
 
 
 class BankAccaunt(Base):
     name: Mapped[str]
-    bik: Mapped[int]
-    paymant_account: Mapped[str]
-    cor_account: Mapped[str]
+    bic: Mapped[int] = mapped_column(BigInteger)
+    address: Mapped[str]
 
     agency_id: Mapped[int] = mapped_column(ForeignKey("agency.id", ondelete="CASCADE"))
     agency: Mapped["Agency"] = relationship(back_populates="bank_accaunt")
