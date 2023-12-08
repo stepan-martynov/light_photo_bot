93f871b35ccb24ddc86a7d67f57e490edd1148f6
stepan-martynov <smartd88@gmail.com>
2023-12-08 17:49:00 +0300

    oop flow

--- src/api/dadata/api_requests.py
+++ src/api/dadata/api_requests.py
@@ -9,47 +9,64 @@ from src.db.database import *
 from src.db.models import Agency, Manager, BankAccaunt
 
 
-dadata = Dadata(config.dadata.token)
+class DadataExt(Dadata):
+
+    def _get_raw_agency_data(self, inn: str) -> dict:
+        try:
+            return self.find_by_id("party", inn)[0]['data']
+        except Exception as e:
+            print(e)
+
+    def _get_raw_bank_data(self, bik: str) -> dict:
+        try:
+            return self.find_by_id("bank", bik)[0]['data']
+        except Exception as e:
+            print(e)
+
+    def get_agency(self, raw_data: dict) -> dict:
+        agency = {}
+        agency['name'] = raw_data['name']['full']
+        agency['inn'] = int(raw_data['inn'])
+        agency['kpp'] = int(raw_data['kpp'])
+        agency['ogrn'] = int(raw_data['ogrn'])
+        agency['opf_short'] = raw_data['opf']['short']
+        agency['opf_full'] = raw_data['opf']['full']
+        agency['address'] = raw_data['address']['unrestricted_value']
+        return agency
+
+    def get_manager(self, raw_data: dict) -> dict:
+        manager = {}
+        manager['full_name'] = raw_data['management']['name']
+        manager['post'] = raw_data['management']['post']
+        return manager
+
+    def get_company(self, inn: str) -> (dict, dict):
+        raw_data = self._get_raw_agency_data(inn)
+        return self.get_agency(raw_data), self.get_manager(raw_data)
+
+    def get_bank_accaunt(self, bik: str) -> dict:
+        raw_data = self._get_raw_bank_data(bik)
+        bank_accaunt = {}
+        bank_accaunt['name'] = raw_data['name']['payment']
+        bank_accaunt['bic'] = int(raw_data['bic'])
+        bank_accaunt['address'] = raw_data['address']['unrestricted_value']
+        return bank_accaunt
 
 
-def get_raw_data(inn: str) -> dict:
-    try:
-        return dadata.find_by_id("party", inn)[0]['data']
-    except Exception as e:
-        print(e)
-
-
-def get_agency(raw_data: dict) -> dict:
-
-    agency = {}
-    agency['name'] = raw_data['name']['full']
-    agency['inn'] = int(raw_data['inn'])
-    agency['kpp'] = int(raw_data['kpp'])
-    agency['ogrn'] = int(raw_data['ogrn'])
-    agency['opf_short'] = raw_data['opf']['short']
-    agency['opf_full'] = raw_data['opf']['full']
-    agency['address'] = raw_data['address']['unrestricted_value']
-    return agency
-
-
-def get_manager(raw_data: dict) -> dict:
-    manager = {}
-    manager['full_name'] = raw_data['management']['name']
-    manager['post'] = raw_data['management']['post']
-    return manager
+dadata_connection = DadataExt(config.dadata.token)
 
 
 async def main():
     await create_tables(async_engine=async_engine)
-    raw_data = get_raw_data("7839113181")
-    # pprint.pprint(raw_data)
-    agency = get_agency(raw_data)
-    manager = get_manager(raw_data)
+    agency, manager = dadata_connection.get_company("7841386500")
+    bank = dadata_connection.get_bank_accaunt("044030706")
     async with asyng_session_factory() as session:
         agency = Agency(**agency)
         manager = Manager(**manager)
+        bank_accaunt = BankAccaunt(**bank)
         agency.manager = manager
-        session.add_all((agency, manager))
+        agency.bank_accaunt = bank_accaunt
+        session.add_all((agency, manager, bank_accaunt))
         await session.commit()
 
 
