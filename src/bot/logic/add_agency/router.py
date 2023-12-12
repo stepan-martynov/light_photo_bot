2b86aa9908c821afde1027dd611e18369d8a65ec
stepan-martynov <smartd88@gmail.com>
2023-12-13 01:44:38 +0300

    maybe finish router

--- src/bot/logic/add_agency/router.py
+++ src/bot/logic/add_agency/router.py
@@ -1,10 +1,13 @@
+import pprint
 from re import Match
 from aiogram.filters import StateFilter
 from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 
-from ...structure.fsm.add_agency import RegisterAgency
-from ....api.dadata.api_requests import dadata_connection
+from src.db.models.agency import Agency
+
+from src.bot.structure.fsm.add_agency import RegisterAgency
+from src.api.dadata.api_requests import dadata_connection
 
 add_agency_router = Router()
 
@@ -22,12 +25,14 @@ async def add_agency(callback: types.CallbackQuery, state: FSMContext):
 async def add_agency(message: types.Message, state: FSMContext, inn: Match[str]):
     try:
         agency, manager = dadata_connection.get_company(str(inn.group(0)))
+        await state.update_data(agency=agency)
+        await state.update_data(manager=manager)
     except:
         return await message.answer("Компания не найдена. Возможно неправильно введен ИНН")
     await state.set_state(RegisterAgency.bik)
     return await message.answer(f"Вы прислали ИНН {str(inn.group(0))}.\
         Мы нашли следующую компанию: {agency}.\
-        Имя менеджера: {manager}.\
+        И менеджера: {manager}.\
         Пришлите БИК")
 
 
@@ -35,11 +40,23 @@ async def add_agency(message: types.Message, state: FSMContext, inn: Match[str])
     RegisterAgency.bik,
     F.text.regexp(r"\d{9}").as_("bik"),
 )
-async def add_agency(message: types.Message, state: FSMContext, bik: Match[str]):
-    await state.clear()
+async def add_agency_bank(message: types.Message, state: FSMContext, bik: Match[str]):
     try:
         bank = dadata_connection.get_bank_accaunt(str(bik.group(0)))
+        await state.update_data(bank=bank)
     except:
         return await message.answer("Банк не найден. Возможно неправильно введен БИК")
-    return await message.answer(f"Неплохо {str(bik.group(0))}.\
-        Ваш банк {bank}.")
+    await state.set_state(RegisterAgency.paymant_account)
+    return await message.answer(f"Ваш банк {bank}. Введите счет для оплаты (20 цифр)")
+
+
+@add_agency_router.message(
+    RegisterAgency.paymant_account,
+    F.text.regexp(r"\d{20}").as_("paymant_account"),
+)
+async def add_bank_account(message: types.Message, state: FSMContext, paymant_account: Match[str]):
+    await state.update_data(paymant_account=paymant_account.group(0))
+    all_data = await state.get_data()
+    pprint.pprint(all_data)
+    state.clear()
+    return await message.answer(f"мы сохранили агенство{all_data}")
