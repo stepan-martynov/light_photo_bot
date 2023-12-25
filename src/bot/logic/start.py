4178fb544200bf00a2e5d5b11b3488099b04f435
stepan-martynov <smartd88@gmail.com>
2023-12-25 19:30:24 +0300

    -

--- src/bot/logic/start.py
+++ src/bot/logic/start.py
@@ -2,7 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
-from ..structure.kb import START_MENU, ADD_AGENCY_MENU
+from ..structure.kb import START_MENU, STSRT_MENU
 
 
 start_router = Router(name="start")
@@ -11,4 +11,4 @@ start_router = Router(name="start")
 @start_router.message(CommandStart())
 async def start(message: types.Message, state: FSMContext):
     await state.clear()
-    return await message.answer("Hi!", reply_markup=ADD_AGENCY_MENU)
+    return await message.answer("Hi!", reply_markup=STSRT_MENU)
