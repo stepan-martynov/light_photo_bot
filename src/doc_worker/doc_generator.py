f087a8e316fb421daeb424e83306da940315779d
stepan-martynov <smartd88@gmail.com>
2025-04-11 20:56:09 +0300

    Изменил структуру конечного dict

--- src/doc_worker/doc_generator.py
+++ src/doc_worker/doc_generator.py
@@ -4,23 +4,24 @@ from docxtpl import DocxTemplate
 from num2words import num2words
 from src.db.models import Photosession, Contract
 
-#TODO add function to create dict from Contract
-def serialize_contract_dict(contract: Contract) -> dict:
-    contract_dict = {
 
+def serialize_contract_dict(contract: Contract) -> dict:
+    """Создает словарь из объекта Contract для шаблонизации документа."""
+    contract_dict = {
+        'name': contract.name,
+        'date': contract.date
     }
     return contract_dict
 
 
-
 async def serialize_photosession_dict(photosession: Photosession) -> dict:
+    """Создает словарь с данными фотосессии для шаблонизации документа."""
     print('++++' * 30)
-    pprint(photosession.contract.photographer.__dict__)
     photosession_dict = {
         'photographer_fullname': photosession.contract.photographer.full_name,
-        'photographer_adress': photosession.contract.photographer.adress,
-        # 'photographer_opf_full': photosession.contract.photographer.opf_full,
-        'photographer_kpp': photosession.contract.photographer.kpp,
+        'photographer_address': photosession.contract.photographer.address,
+        'photographer_opf_full': photosession.contract.photographer.opf_full,
+        'photographer_opf_short': photosession.contract.photographer.opf_short,
         'photographer_ogrnip': photosession.contract.photographer.ogrnip,
         'photographer_inn': photosession.contract.photographer.inn,
         'photographer_bank_acc': photosession.contract.photographer.paymant_account,
@@ -32,29 +33,30 @@ async def serialize_photosession_dict(photosession: Photosession) -> dict:
         'agency_inn': photosession.contract.agency.inn,
         'agency_kpp': photosession.contract.agency.kpp,
         'agency_ogrn': photosession.contract.agency.ogrn,
-        'agency_adress': photosession.contract.agency.address,
-        'agency_bank_adress': photosession.contract.agency.bank_accaunt.address,
+        'agency_address': photosession.contract.agency.address,
+        'agency_bank_address': photosession.contract.agency.bank_accaunt.address,
         'agency_corr': photosession.contract.agency.bank_accaunt.correspondent_account,
         'agency_bank_acc': photosession.contract.agency.paymant_account,
-        'manager_full_name': photosession.contract.agency.manager.initials,
-        # 'manager_short': photosession.contract.agency.manager.short_initials,
+        'manager_full_name': photosession.contract.agency.manager.full_name,
+        'manager_short': f'{photosession.contract.agency.manager.last_name} {photosession.contract.agency.manager.initials}',
         'contract_name': photosession.contract.name,
         'contract_date': photosession.contract.date,
-        # 'upper_bill_number': photosession.contract.upper_bill_number,
-        # 'bill_date': photosession.contract.bill_date,
-        # 'bill_num': photosession.contract.bill_num,
+        'bill_num': photosession.order_number,
+        'upper_bill_number': photosession.upper_order_number,
+        'bill_date': photosession.date,
         'service_name': photosession.service.name,
-        'photosession_price': photosession.price,
-        'photosession_price_litteral': num2words(photosession.price, lang="ru"),
-        'photosession_adress': photosession.location,
+        'price': photosession.price,
+        'price_litteral': photosession.price_litteral,
+        'photosession_address': photosession.location,
         'photosession_url': photosession.url,
-        'cur_date': photosession.date
+        'photosession_date': photosession.date
     }
     pprint(photosession_dict)
     return photosession_dict
 
 
 def render_docx(photosession: Photosession) -> str:
+    """Создает документ Word на основе шаблона для фотосессии."""
     doc_tamplate = f'{photosession.contract.agency.name}.docx'
     docx_tamplate_path = os.path.join('templates', doc_tamplate)
 
